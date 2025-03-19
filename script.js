aebc15373c86bf5c5db8335d04c94bc4e8733d37
lebuckman <liambuckman0@gmail.com>
2025-03-18 17:53:08 -0700

    Set global variables and test playRound()

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let computerScore = 0;
+let humanScore = 0;
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+playRound(humanSelection, computerSelection);
+
+function playRound(humanChoice, computerChoice) {
+    console.log(humanChoice);
+    console.log(computerChoice);
+}
+
 function getComputerChoice() {
     let computerChoice;
     const randomNum = Math.floor(Math.random() * 3) + 1;
